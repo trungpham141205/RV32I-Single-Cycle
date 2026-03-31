e90d61c8f765c1b5f0152910d58f3e9f1f414ec9
trungpham141205 <pquoctrung141205@gmail.com>
2026-03-31 10:43:15 +0700

    new

---
+++ src/core/control_unit.v
@@ -0,0 +1,27 @@
+module control_unit(
+    input [6:0]opcode,
+    output reg RegWrite, ALUSrc, MemWrite, Branch, Jump,
+    output reg[1:0]ResultSrc, ALUOp
+);
+
+    always @(*) begin
+        case (opcode)
+            7'b011_0111: {RegWrite, ALUSrc, MemWrite, ResultSrc, Branch, ALUOp, Jump} = 9'b1x0110xx0; // LUI
+            7'b001_0111: {RegWrite, ALUSrc, MemWrite, ResultSrc, Branch, ALUOp, Jump} = 9'b110110000; // AUIPC
+
+            7'b110_1111: {RegWrite, ALUSrc, MemWrite, ResultSrc, Branch, ALUOp, Jump} = 9'b1x0100xx1; // JAL
+            7'b110_0111: {RegWrite, ALUSrc, MemWrite, ResultSrc, Branch, ALUOp, Jump} = 9'b110100000; // JALR
+
+            7'b110_0011: {RegWrite, ALUSrc, MemWrite, ResultSrc, Branch, ALUOp, Jump} = 9'b000xx1010; // Branch
+
+            7'b000_0011: {RegWrite, ALUSrc, MemWrite, ResultSrc, Branch, ALUOp, Jump} = 9'b110010000; // LW
+            7'b010_0011: {RegWrite, ALUSrc, MemWrite, ResultSrc, Branch, ALUOp, Jump} = 9'b011xx0000; // SW
+
+            7'b011_0011: {RegWrite, ALUSrc, MemWrite, ResultSrc, Branch, ALUOp, Jump} = 9'b100000100; // R type
+            7'b001_0011: {RegWrite, ALUSrc, MemWrite, ResultSrc, Branch, ALUOp, Jump} = 9'b110000100; // I-type ALU
+
+            default: {RegWrite, ALUSrc, MemWrite, ResultSrc, Branch, ALUOp, Jump} = 9'b000000000;
+        endcase
+    end
+
+endmodule
--- src/core/immediate_generate.v
+++ src/core/immediate_generate.v
@@ -9,15 +9,15 @@ module immediate_generate(
             7'b011_0111: immediate_extend = {instruction[31:12], 12'b0}; // LUI
             7'b001_0111: immediate_extend = {instruction[31:12], 12'b0}; // AUIPC
 
-            7'b110_1111: immediate_extend = {{12{instruction[31]}}, instruction[19:12], instruction[20], instruction[30:21], 1'b0}; //JAL
-            7'b110_0111: immediate_extend = {{20{instruction[31]}}, instruction[30:20]}; // JALR
+            7'b110_1111: immediate_extend = {{12{instruction[31]}}, instruction[19:12], instruction[20], instruction[30:21], 1'b0}; // JAL
+            7'b110_0111: immediate_extend = {{20{instruction[31]}}, instruction[31:20]}; // JALR
 
             7'b110_0011: immediate_extend = {{20{instruction[31]}}, instruction[7], instruction[30:25], instruction[11:8], 1'b0}; // BEQ, BNE
 
-            7'b000_0011: immediate_extend = {{20{instruction[31]}}, instruction[30:20]}; // LW
-            7'b010_0011: immediate_extend = {{20{instruction[31]}}, instruction[30:25], instruction[11:8], instruction[7]}; //SW
+            7'b000_0011: immediate_extend = {{20{instruction[31]}}, instruction[31:20]}; // LW
+            7'b010_0011: immediate_extend = {{20{instruction[31]}}, instruction[31:25], instruction[11:7]}; //SW
 
-            7'b001_0011: immediate_extend = {{20{instruction[31]}}, instruction[30:20]}; // ADDI
+            7'b001_0011: immediate_extend = {{20{instruction[31]}}, instruction[31:20]}; // ADDI
             default: immediate_extend = 32'b0;
         endcase
     end
