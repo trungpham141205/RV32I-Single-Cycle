19778c1a8761f3e00f91fa93b92d9b9f8a6fe445
trungpham141205 <pquoctrung141205@gmail.com>
2026-04-06 20:55:25 +0700

    ver 1

--- src/core/immediate_generate.v
+++ src/core/immediate_generate.v
@@ -6,28 +6,31 @@ module immediate_generate(
 
     always @(*) begin
         case (opcode)
-            7'b011_0111: immediate_extend = {instruction[31:12], 12'b0}; // LUI
-            7'b001_0111: immediate_extend = {instruction[31:12], 12'b0}; // AUIPC
+            // U Type
+            7'b011_0111, // Load Upper Immediate (LUI)
+            7'b001_0111: // Add Upper Immediate to PC (AUIPC)
+                immediate_extend = {instruction[31:12], 12'b0}; 
+            
+            // J Type
+            7'b110_1111: // Jump and Link (JAL)
+                immediate_extend = {{11{instruction[31]}}, instruction[31], instruction[19:12], instruction[20], instruction[30:21], 1'b0}; 
 
-            7'b110_1111: immediate_extend = {{12{instruction[31]}}, instruction[19:12], instruction[20], instruction[30:21], 1'b0}; // JAL
-            7'b110_0111: immediate_extend = {{20{instruction[31]}}, instruction[31:20]}; // JALR
+            // B Type
+            7'b110_0011: // Branch Instructions
+                immediate_extend = {{19{instruction[31]}}, instruction[31], instruction[7], instruction[30:25], instruction[11:8], 1'b0}; 
+            
+            // I Type
+            7'b110_0111, // Jump and Link Register (JALR)
+            7'b000_0011, // Memory Load Instructions
+            7'b001_0011: // Integer Register - Immediate Instructions
+                immediate_extend = {{20{instruction[31]}}, instruction[31:20]}; 
 
-            7'b110_0011: immediate_extend = {{20{instruction[31]}}, instruction[7], instruction[30:25], instruction[11:8], 1'b0}; // BEQ, BNE
+            // S Type
+            7'b010_0011: // Memory Store Instructions
+                immediate_extend = {{20{instruction[31]}}, instruction[31:25], instruction[11:7]}; 
 
-            7'b000_0011: immediate_extend = {{20{instruction[31]}}, instruction[31:20]}; // LW
-            7'b010_0011: immediate_extend = {{20{instruction[31]}}, instruction[31:25], instruction[11:7]}; //SW
-
-            7'b001_0011: immediate_extend = {{20{instruction[31]}}, instruction[31:20]}; // ADDI
             default: immediate_extend = 32'b0;
         endcase
     end
 
 endmodule
-
-// add, sub, addi
-// and, or, xor
-// lw, sw
-// beq, bne
-// slt
-// jal, jalr
-// auipc
