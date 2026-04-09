6a190b555af66bea404a1c710fe0155707159f62
trungpham141205 <pquoctrung141205@gmail.com>
2026-04-09 14:47:35 +0700

    version 1.0

--- src/core/alu_control.v
+++ src/core/alu_control.v
@@ -13,29 +13,29 @@ module alu_control(
             //──────────────────────────────────────\\
             //─────────────── R Type ───────────────\\
             //──────────────────────────────────────\\
-            6'b100000: ALUControl = 4'b0000; // ADD, ADDI
+            6'b100000: ALUControl = 4'b0000; // ADD
             6'b100001: ALUControl = 4'b0001; // SUB
-            6'b100010: ALUControl = 4'b0010; // SLL, SLLI
-            6'b100100: ALUControl = 4'b0011; // SLT, SLTI
-            6'b100110: ALUControl = 4'b0100; // SLTU, SLTIU
-            6'b101000: ALUControl = 4'b0101; // XOR, XORI
-            6'b101010: ALUControl = 4'b0110; // SRL, SRLI
-            6'b101011: ALUControl = 4'b0111; // SRA, SRAI
-            6'b101100: ALUControl = 4'b1000; // OR, ORI
-            6'b101110: ALUControl = 4'b1001; // AND, ANDI
+            6'b10001?: ALUControl = 4'b0010; // SLL
+            6'b10010?: ALUControl = 4'b0011; // SLT
+            6'b10011?: ALUControl = 4'b0100; // SLTU
+            6'b10100?: ALUControl = 4'b0101; // XOR
+            6'b101010: ALUControl = 4'b0110; // SRL
+            6'b101011: ALUControl = 4'b0111; // SRA
+            6'b10110?: ALUControl = 4'b1000; // OR
+            6'b10111?: ALUControl = 4'b1001; // AND
 
             //──────────────────────────────────────\\
             //─────────────── I Type ───────────────\\
             //──────────────────────────────────────\\
-            6'b110000: ALUControl = 4'b0000; // ADDI
+            6'b11000?: ALUControl = 4'b0000; // ADDI
             6'b110010: ALUControl = 4'b0010; // SLLI
-            6'b110100: ALUControl = 4'b0011; // SLTI
-            6'b110110: ALUControl = 4'b0100; // SLTIU
-            6'b111000: ALUControl = 4'b0101; // XORI
+            6'b11010?: ALUControl = 4'b0011; // SLTI
+            6'b11011?: ALUControl = 4'b0100; // SLTIU
+            6'b11100?: ALUControl = 4'b0101; // XORI
             6'b111010: ALUControl = 4'b0110; // SRLI
             6'b111011: ALUControl = 4'b0111; // SRAI
-            6'b111100: ALUControl = 4'b1000; // ORI
-            6'b111110: ALUControl = 4'b1001; // ANDI
+            6'b11110?: ALUControl = 4'b1000; // ORI
+            6'b11111?: ALUControl = 4'b1001; // ANDI
 
             default: ALUControl = 4'b0;
         endcase
