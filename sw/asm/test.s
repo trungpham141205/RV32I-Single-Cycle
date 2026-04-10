98726bdc434daec8405432bc0d778ea7db5e702c
trungpham141205 <pquoctrung141205>
2026-04-10 15:15:09 +0700

    version 1.0

--- sw/asm/test.s
+++ sw/asm/test.s
@@ -1,18 +1,7 @@
-addi x1, x0, 5 
-addi x2, x0, 3
-add x3, x1, x2
-sub x4, x1, x2
-and x5, x1, x2
-or x6, x1, x2
-xor x7, x1, x2
-slt x8, x2, x1
-sw x3, 0(x0)
-lw x9, 0(x0)
-beq x1, x1, skip
-addi x10, x0, 99
-skip:
-addi x10, x0, 42
-jal x11, done
-addi x11, x0, 99
-done:
-addi x0, x0, 0
+.global _start
+
+.text
+_start:
+	li x1, 5
+	li x2, 6
+	add x3, x1, x2
